0d21a839fb0825a6fe7d5d8098dfdbadb289af3c
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2021-05-15 22:59:32 +0530

    email

--- services/passport.js
+++ services/passport.js
@@ -2,6 +2,10 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
+const MailerTwo = require('../services/MailerTwo')
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
+
+
 
 const User = mongoose.model('users');
 
@@ -24,13 +28,36 @@ passport.use(
       proxy: true
     },
     async (access, refreshToken, profile, done) => {
-      const existingUser = await User.findOne({ googleId: profile.id });
+      console.log(profile.emails[0].value)
+      const existingUser = await User.findOne({ googleId: profile.emails[0].value });
 
       if (existingUser) {
         return done(null, existingUser);
       }
 
-      const user = await new User({ googleId: profile.id }).save();
+      const survey = {
+        title : "Welcome to our website",    // identical value can be condensed in ES6
+        subject : "Welcome",
+        body : "Welcome",
+        recipients : [profile.emails[0].value],
+    }
+
+      const user = await new User({ googleId: profile.emails[0].value }).save();
+      const mailer = new MailerTwo(survey , surveyTemplate(survey))
+
+      try{
+        mailer.send()
+        // await survey.save()
+        // req.user.credits -= 1
+        // const user = await req.user.save()
+
+        // res.send(user)
+        console.log("mail Send")
+    }
+    catch (err){
+        "Mail Not Send"
+    }
+
       done(null, user);
     }
   )
