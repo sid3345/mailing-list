dc708d6da19656edac0d35d7b10b5968fccd1e39
Siddharth Sinha <sid3345@gmail.com>
2021-09-13 00:17:05 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,11 +1,11 @@
 // don't commit this file
 
 module.exports = {
-    googleClientID : '334847611372-vs5fadi5mnfl981d5n7ee23cct7sksib.apps.googleusercontent.com',
-    googleClientSecret : '-2fLQ2Y39MPjFN1yID9ofsxR',
-    mongoURI : 'mongodb+srv://myuser:fa6K8MSMNLYD82R0@cluster0.adnqn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    cookieKey : 'hvdwbcibwecvwnckewjvewbcwejkbvwehbvewnjwebvhwev',
+    googleClientID : '',
+    googleClientSecret : '',
+    mongoURI : 'mongodb+srv://myuser:<user ID>@<cluster>/DB',
+    cookieKey : '',
    
-    sendGridKey : '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',
+    sendGridKey : '',
     redirectDomain : 'http://localhost:3000'
 }
